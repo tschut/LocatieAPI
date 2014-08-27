305663929d8e51eb4455627b8d0d1d7c705af329
tschut <tschut@gmail.com>
2014-08-27 20:28:06 +0200

    produce application/json

--- lokatieapi/src/main/java/nl/loxia/lokatieapi/LokatieService.java
+++ lokatieapi/src/main/java/nl/loxia/lokatieapi/LokatieService.java
@@ -3,12 +3,14 @@ package nl.loxia.lokatieapi;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 @Path("/lokatieapi")
 public class LokatieService {
     @GET
     @Path("/geocode-en-km/{geocode}/{km}")
+    @Produces({ "application/json" })
     public Response helloWorld(@PathParam("geocode") String geocode, @PathParam("km") String km) {
         String hello = "Hello world! " + geocode + ":" + km;
         return Response.ok(hello).build();
