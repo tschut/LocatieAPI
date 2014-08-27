71264ebbe6150d2ce26a2fb8848ed2df8be4cde9
tschut <tschut@gmail.com>
2014-08-27 20:22:54 +0200

    Interface /geocode-en-km/ toegevoegd

--- lokatieapi/src/main/java/nl/loxia/lokatieapi/LokatieService.java
+++ lokatieapi/src/main/java/nl/loxia/lokatieapi/LokatieService.java
@@ -8,9 +8,9 @@ import javax.ws.rs.core.Response;
 @Path("/lokatieapi")
 public class LokatieService {
     @GET
-    @Path("/helloworld/{test}")
-    public Response helloWorld(@PathParam("test") String test) {
-        String hello = "Hello world! " + test;
+    @Path("/geocode-en-km/{geocode}/{km}")
+    public Response helloWorld(@PathParam("geocode") String geocode, @PathParam("km") String km) {
+        String hello = "Hello world! " + geocode + ":" + km;
         return Response.ok(hello).build();
     }
 }
