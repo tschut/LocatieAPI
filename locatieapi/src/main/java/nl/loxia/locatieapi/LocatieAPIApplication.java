8154f29531245066b08c8a2d5d7d4c7976411f7a
tschut <tschut@gmail.com>
2014-09-03 19:26:04 +0200

    Add Htmleasy providers and servletdispatcher

--- locatieapi/src/main/java/nl/loxia/locatieapi/LocatieAPIApplication.java
+++ locatieapi/src/main/java/nl/loxia/locatieapi/LocatieAPIApplication.java
@@ -5,11 +5,15 @@ import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
+import com.googlecode.htmleasy.HtmleasyProviders;
+
 public class LocatieAPIApplication extends Application {
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> result = new HashSet<Class<?>>();
         result.add(nl.loxia.locatieapi.LocatieService.class);
+
+        result.addAll(HtmleasyProviders.getClasses());
 
         return result;
     }
